291d99943f9705e867faa1e41ea6f88022af016b
rimtimti <rimtimti@mail.ru>
2022-06-11 14:17:56 +0300

    задача 2 без проверки ввода

---
+++ c#_Lesson001_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+Console.Clear();
+Console.WriteLine("Это программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+//Здесь бы сделать проверку, возможно, введено нецелое число или текст, но я пока не знаю как
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+//Здесь бы сделать проверку, возможно, введено нецелое число или текст, но я пока не знаю как
+Console.WriteLine("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+//Здесь бы сделать проверку, возможно, введено нецелое число или текст, но я пока не знаю как
+int max = number1;
+if (number2 > max)
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+Console.Write("Самое большое из этих чисел: "); Console.WriteLine(max);

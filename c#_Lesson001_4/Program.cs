0d60a830d3d0ed4dbb5a4d948bad500d011c40ba
rimtimti <rimtimti@mail.ru>
2022-06-11 15:01:32 +0300

    задача 4 без проверки ввода

---
+++ c#_Lesson001_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+Console.Clear();
+Console.WriteLine("Это программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+//Здесь бы сделать проверку, возможно, введено нецелое число или текст, но я пока не знаю как
+if (number < 0)
+{
+    Console.WriteLine("Вы ввели отрицательное число!");
+}
+Console.WriteLine();
+int x = 2;
+while (x <= number)
+{
+    Console.WriteLine(x);
+    x = x + 2;
+}

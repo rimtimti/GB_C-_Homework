79a66fcbc0c40469ea9dcf781b8714843f34158b
rimtimti <rimtimti@mail.ru>
2022-06-11 14:31:24 +0300

    задача 3 без проверки ввода

---
+++ c#_Lesson001_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.Clear();
+Console.WriteLine("Это программа, которая на вход принимает число и выдаёт, является ли число чётным.");
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+//Здесь бы сделать проверку, возможно, введено нецелое число или текст, но я пока не знаю как
+if (number %2 == 0)
+{
+    Console.WriteLine("Это число четное");
+}
+else
+{
+    Console.WriteLine("Это число нечетное");
+}

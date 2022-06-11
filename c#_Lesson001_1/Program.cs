4e10b159fd6927fca0f02b309f1cc460b8f4ec49
rimtimti <rimtimti@mail.ru>
2022-06-11 11:38:37 +0300

    задача 1 без провеки ввода

---
+++ c#_Lesson001_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.Clear();
+Console.WriteLine("Это программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.Write (number1); Console.Write (" больше "); Console.WriteLine (number2);
+}
+else
+{
+    Console.Write (number2); Console.Write (" больше "); Console.WriteLine (number1);
+}
+    

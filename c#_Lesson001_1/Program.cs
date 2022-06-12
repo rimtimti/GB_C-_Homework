20dcd7acdcdc2547a439ef3e4d7f4f42a637c9da
rimtimti <rimtimti@mail.ru>
2022-06-12 21:28:44 +0300

    задача 1

--- c#_Lesson001_1/Program.cs
+++ c#_Lesson001_1/Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 
 Console.Clear();
-Console.WriteLine("Это программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
+Console.WriteLine("Это программа, которая на вход принимает два целых числа и выдаёт, какое число большее, а какое меньшее.");
+int number1;
 Console.WriteLine("Введите первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
+if (!int.TryParse(Console.ReadLine(), out number1))
+    {
+    Console.Write("Ошибка ввода числа!");
+    return;
+    }
+int number2;
 Console.WriteLine("Введите второе число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+if (!int.TryParse(Console.ReadLine(), out number2))
+    {
+    Console.Write("Ошибка ввода числа!");
+    return;
+    }
 if (number1 > number2)
 {
-    Console.Write (number1); Console.Write (" больше "); Console.WriteLine (number2);
+    Console.Write(number1); Console.Write(" больше "); Console.WriteLine(number2);
 }
 else
 {
-    Console.Write (number2); Console.Write (" больше "); Console.WriteLine (number1);
+    Console.Write(number2); Console.Write(" больше "); Console.WriteLine(number1);
 }
-    

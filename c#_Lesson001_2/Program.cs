616b9d11e59b3c16145ddd6a59becca84f619733
rimtimti <rimtimti@mail.ru>
2022-06-12 21:29:09 +0300

    задача 2

--- c#_Lesson001_2/Program.cs
+++ c#_Lesson001_2/Program.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 
 Console.Clear();
-Console.WriteLine("Это программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+Console.WriteLine("Это программа, которая принимает на вход три целых числа и выдаёт максимальное из этих чисел.");
+int number1;
 Console.WriteLine("Введите первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-//Здесь бы сделать проверку, возможно, введено нецелое число или текст, но я пока не знаю как
+if (!int.TryParse(Console.ReadLine(), out number1))
+    {
+    Console.Write("Ошибка ввода числа!");
+    return;
+    }
+int number2;
 Console.WriteLine("Введите второе число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
-//Здесь бы сделать проверку, возможно, введено нецелое число или текст, но я пока не знаю как
+if (!int.TryParse(Console.ReadLine(), out number2))
+    {
+    Console.Write("Ошибка ввода числа!");
+    return;
+    }
+int number3;
 Console.WriteLine("Введите третье число: ");
-int number3 = Convert.ToInt32(Console.ReadLine());
-//Здесь бы сделать проверку, возможно, введено нецелое число или текст, но я пока не знаю как
+if (!int.TryParse(Console.ReadLine(), out number3))
+    {
+    Console.Write("Ошибка ввода числа!");
+    return;
+    }
 int max = number1;
 if (number2 > max)
 {

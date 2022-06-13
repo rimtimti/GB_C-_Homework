00c305abc9dccca805f948abaefc57dbdc10293a
rimtimti <rimtimti@mail.ru>
2022-06-13 12:28:31 +0300

    задача 3

---
+++ c#_Lesson002_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+Console.Clear();
+Console.WriteLine("Это программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+int number;
+Console.WriteLine("Введите номер дня недели: ");
+if (!int.TryParse(Console.ReadLine(), out number))
+    {
+    Console.Write("Ошибка ввода числа!");
+    return;
+    }
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Такого дня недели нет!");
+    return;
+}
+else if (number == 6 || number == 7)
+{
+    Console.WriteLine("Ура, это выходной!");
+}
+else
+{
+    Console.WriteLine("Это рабочий день");
+}

2492a2e45555ca90dc1c68446e36ab148717b4bf
rimtimti <rimtimti@mail.ru>
2022-06-13 12:27:46 +0300

    задача 1

--- c#_Lesson002_1/Program.cs
+++ c#_Lesson002_1/Program.cs
@@ -3,18 +3,18 @@
 Console.Clear();
 Console.WriteLine("Это программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
 int number;
-Console.WriteLine("Введите целое положительное трехзначное число: ");
+Console.WriteLine("Введите целое трехзначное число: ");
 if (!int.TryParse(Console.ReadLine(), out number))
     {
     Console.Write("Ошибка ввода числа!");
     return;
     }
-if (number < 100 || number > 999)
+if ((Math.Abs(number) < 100) || (Math.Abs(number) > 999))
 {
     Console.WriteLine("Вы ввели некорректное число!");
     return;
 }
 
-int x = (number / 10) % 10;
+int x = (Math.Abs(number) / 10) % 10;
 Console.WriteLine("Вторая цифра этого числа:");
 Console.WriteLine(x);

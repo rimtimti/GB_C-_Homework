d7f2a3046db0d6ca1f2a3941ee67009b6b47cc4a
rimtimti <rimtimti@mail.ru>
2022-06-16 21:11:17 +0300

    задача 3

---
+++ c#_Lesson003_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+Console.Clear();
+Console.WriteLine("Это программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.");
+int number;
+Console.Write("Введите положительное целое число: ");
+if (!int.TryParse(Console.ReadLine(), out number))
+    {
+    Console.Write("Ошибка ввода числа!");
+    return;
+    }
+if (number < 1)
+{
+    Console.WriteLine("Вы ввели не то число!");
+    return;
+}
+for (int x = 1; x <= number; x++)
+{
+    Console.WriteLine(Math.Pow(x,3));
+}
